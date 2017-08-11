5376719c9af038cba0f17681eded7f071e3de1bd
Senya Webster <senyawebster@Senyas-MacBook-Air-2.local>
2017-08-11 15:55:02 -0700

    add track suggestion functionality using array tied to form;  was working before when following page but now broken again—go back to simple branching?

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,56 @@
 
 //Back End —————————————————————————————————————
+function RubyRails(scale, stack, nativeness){
+  var suggested_track;
 
+  // set suggested track based on user's input
+  if (scale == "enterprise" && stack == "backend") {
+    // set suggested track based on condition
+    suggested_track = "You should work on backend enterprise level software!";
+  } else if (scale == "enterprise" && stack == "frontend") {
+    // set suggested track based on condition
+    suggested_track = "You should work on frontend enterprise level software!";
+  } else {
+    suggested_track = "No matching track";
+  }
+
+  return suggested_track;
+}
+
+//function
 
 
 //Front End ————————————————————————————————————
 $(document).ready(function(){
-  $('#TrackSuggester').submit(function(event)) {
+  $('#TrackSuggester').submit(function(event) {
     event.preventDefault();
 
-    var
-    var
-    var
+    // assumes that the user has checked all radio inputs
+    // fetches all radio input values
+    var checked_values = $('input:checked', '#TrackSuggester').map(function() {
+      console.log("value is " + this.value.toString());
+      return this.value;
+    }).get();
+    #TrackSuggester#name.val();
+    // passes user input to "backend" for processing
+    var message = RubyRails(checked_values[0],
+                            checked_values[1],
+                            checked_values[2]);
 
-    if -- then Ruby/Rails
-    if -- then PHP/Drupal
-    if -- then Java/Android
-    if -- then CSS/Design
-    if -- then C#/.Net
+    // output message to user
+    alert(message);
 
-  }
+    // var RubyRails = $()
+    // var phpDrupal
+    // var javaAndroid
+
+
+    //if -- then Ruby/Rails
+    //if -- then PHP/Drupal
+    //if -- then Java/Android
+    //if -- then CSS/Design
+    //if -- then C#/.Net
+
+  });
 
 });
