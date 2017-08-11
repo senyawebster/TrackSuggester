7539e78fb6232af7b88e29e8a25906e3fe1b469d
Senya Webster <senyawebster@Senyas-MacBook-Air-2.local>
2017-08-11 10:54:44 -0700

    add all directories and files and begin inputting form questions

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+
+//Back End —————————————————————————————————————
+
+
+
+//Front End ————————————————————————————————————
+$(document).ready(function(){
+  $('#TrackSuggester').submit(function(event)) {
+    event.preventDefault();
+
+    var
+    var
+    var
+
+    if -- then Ruby/Rails
+    if -- then PHP/Drupal
+    if -- then Java/Android
+    if -- then CSS/Design
+    if -- then C#/.Net
+
+  }
+
+});
