12d63fc33c07e88f8da34ad05c17f0817269792f
Senya Webster <senyawebster@Senyas-MacBook-Air-2.local>
2017-08-20 06:55:48 -0700

    add show and hide features for the form and hidden DIVs plus helpful information for specific tracks within each DIV

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,17 @@ $(document).ready(function(){
 
     if (name != '') {
       if (size === 'big') {
-        alert("Hey " + name + " you should look at our C# and Java courses")
+        alert("Hey " + name + ", you should look at our C# and Java courses")
+        $("form#TrackSuggester").hide();
+        $("div.bigResult").show();
       } else if (size === 'startup') {
-        alert("Hey " + name + " you should look at our Ruby track")
+        alert("Hey " + name + ", you should look at our Ruby track")
+        $("form#TrackSuggester").hide();
+        $("div.startupResult").show();
       } else if (size === 'contract') {
-        alert("Hey " + name + " you should look at our PHP and Design courses")
+        alert("Hey " + name + ", you should look at our PHP and Design courses")
+        $("form#TrackSuggester").hide();
+        $("div.contractResult").show();
       }
     } else {
       alert("please enter your name")
